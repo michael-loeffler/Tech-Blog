23158bdd87effda0e4548018eca6438c00201adb
mloeff <michaelloeffler23@gmail.com>
2023-03-25 12:19:46 -0500

    creates POST route and JavaScript for form handling for new posts

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -5,14 +5,34 @@ const withAuth = require('../../utils/auth');
 //import sequelize
 const sequelize = require('../../config/connection');
 
+router.post('/', async (req, res) => {
+  try {
+    const postData = await Post.create({
+      title: req.body.title,
+      content: req.body.content,
+      user_id: req.session.user_id
+    });
+    res.status(200).json({ message: 'New post successfully created', post: postData });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+})
+
+
+
+
+
+
+
+
 router.get('/', async (req, res) => {
   try {
     const postData = await Post.findAll({
-      include: [{model: Comment, include:[{model: User, attributes: {exclude: ['password']}}]}, {model: User, attributes: { exclude: ['password'] }}],
-      
+      include: [{ model: Comment, include: [{ model: User, attributes: { exclude: ['password'] } }] }, { model: User, attributes: { exclude: ['password'] } }],
+
       //order: [['post_name', 'ASC']]
     })
-    const posts = postData.map((post) => post.get({plain: true}))
+    const posts = postData.map((post) => post.get({ plain: true }))
     res.status(200).json(postData);
   } catch (err) {
     res.status(400).json(err)
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,1 +1,44 @@
 // need to have feature to rerender the page with new data after form submission
+const form = document.querySelector('form');
+// const postRemove = document.getElementsByClassName('postRemove');
+
+const addPost = async (e) => {
+    e.preventDefault();
+
+    const title = document.querySelector('#title').value.trim();
+    const content = document.querySelector('#content').value.trim();
+
+    const response = await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({ title, content }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        form.reset();
+        location.reload();
+    } else {
+        alert('Post was not created successfully');
+    }
+};
+
+// const removeFromWishlist = async (e) => {
+//     const button = e.target;
+//     const product_id = button.getAttribute('id');
+//     const response = await fetch(`/api/users/wishlist/${product_id}`, {
+//         method: 'DELETE'
+//     });
+
+//     if (response.ok) {
+//         button.classList.remove('clicked', 'btn-danger');
+//         button.classList.add('unclicked', 'btn-primary');
+//         button.textContent = "Add to Wishlist";
+//         button.removeEventListener('click', removeFromWishlist)
+//         button.addEventListener('click', addToWishlist);
+//         location.reload();
+//     } else {
+//         alert('Failed to delete wishlist item');
+//     }
+// };
+
+form.addEventListener('submit', addPost);
